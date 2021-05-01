dce9433286906ee6d0dc664f963ae9968bd6d9a8
rjalabado <72759692+rjalabado@users.noreply.github.com>
2021-05-01 13:20:03 -0700

    Initial Push

---
+++ apidoc/api_project.js
@@ -0,0 +1,24 @@
+define({
+  "name": "TCSS 450 Lab Web Service",
+  "version": "0.1.0",
+  "description": "The TCSS 450 Lab Web Service API Documentation",
+  "title": "Lab API Doc",
+  "url": "https://ralabado-tcss450-labs.herokuapp.com/",
+  "order": [
+    "Hello",
+    "Params",
+    "Wait",
+    "DemoSql",
+    "Auth",
+    "Phish"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-27T02:45:00.337Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
